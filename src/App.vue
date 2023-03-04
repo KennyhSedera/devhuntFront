050a88db162460a3036827312de2994370da31bb
KennyhSedera <125634923+KennyhSedera@users.noreply.github.com>
2023-03-04 17:35:57 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <!-- <Navbar v-if="$route.name != 'login'"/> -->
+    <v-main>
+      mandeha
+    </v-main>
+  </v-app>
+</template>
+<script>
+//import Navbar from '@/components/NavBar.vue'
+
+export default {
+  components:{
+   // Navbar
+  },
+  data() {
+    return {
+      // 
+    }
+  },
+}
+</script>
